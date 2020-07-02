01929b71fe560eeab13f14e22971f7451b4f53b2
Yaroslav Kravchenko <yaroslavkravch@gmail.com>
2020-07-02 17:25:23 +0300

    Learn to make http requests

--- app/app.js
+++ app/app.js
@@ -1,84 +1,32 @@
-const fs = require('fs');
-const path = require('path');
-const student = require('./one.json');
-const csv = require('csv-parser');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const http = require('http');
+const url = require('url');
+const { parse } = require('querystring');
 
-/*fs.writeFile('one.txt', 'work', (err) => {
-    if(err) console.log('Error');
-});*/
+http.createServer((request, response) => {
+    console.log('server work');
+    if (request.method === 'GET') {
+        // GET -> получить
+        console.log(request.method); // !!!!
+        let urlRequest = url.parse(request.url, true);
+        //console.log(urlRequest);
+        console.log(urlRequest.query.p);
+        if (urlRequest.query.p  === '70') {
+            response.end('800');
+        }
+        response.end('ne 800');
+    } else {
+        // POST
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            console.log(body);
+            let params = parse(body);
+            console.log(params);
+            console.log(params.hi);
+            response.end('ok');
+        });
+    }
+}).listen(3002);
 
-/**
- * Case 1. Записываем json file
- */
-
-const man = {
-    name: 'Alex',
-    age: 22,
-    department: 'History',
-    car: 'vaz'
-};
-
-/*fs.writeFile('one.json', JSON.stringify(man), (err) => {
-    if(err) console.log('Error');
-});*/
-
-/**
- * Case 2. Читаем json file
- */
-
-//console.log(student);
-
-/**
- * Case 3. Читаем CSV json file
- */
-
-const results = [];
-
-fs.createReadStream('test.csv')
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () =>{
-        console.log(results);
-    });
-
-
-/**
- * case 4. Пишем  CSV file
- */
-
-/*const csvWriter = createCsvWriter({
-    path: 'test.csv',
-    header: [
-        {id: 'name', title: 'NAME'},
-        {id: 'surname', title: 'Last Name'},
-        {id: 'age', title: 'AGE'},
-        {id: 'gender', title: 'GENDER'}
-    ]
-});
-
-const data = [
-  {
-    name: 'John',
-    surname: 'Snow',
-    age: 26,
-    gender: 'M'
-  },
-  {
-    name: 'Clair',
-    surname: 'White',
-    age: 33,
-    gender: 'F'
-  },
-  {
-    name: 'Fancy',
-    surname: 'Brown',
-    age: 78,
-    gender: 'F'
-  }
-];
-
-csvWriter.writeRecords(data)
-    .then(() => {
-      console.log('...Done');
-    });*/
